2faf03136117e4a6fb170df66e81178eb9c2d08e
Shia <rise.shia@gmail.com>
2022-05-01 20:41:47 +0900

    Setup minitest

--- Gemfile
+++ Gemfile
@@ -7,4 +7,6 @@ gemspec
 
 gem "rake", "~> 13.0"
 
+gem "minitest"
+gem "minitest-focus"
 gem "rubocop", "~> 1.21"
---
+++ test/test_simple_web_server.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "helper"
+
+class TestSimpelWebServer < Minitest::Test
+  def test_ok
+    assert true
+  end
+end
