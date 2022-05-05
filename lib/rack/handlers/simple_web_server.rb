a8c03b86f6dd6b007e23d232aa8f7a69b7d903aa
Shia <rise.shia@gmail.com>
2022-05-05 18:22:13 +0900

    register handler

--- lib/rack/handlers/simple_web_server.rb
+++ lib/rack/handlers/simple_web_server.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Rack
+  # Rack::Handler
   module Handler
     # Rack handler for SimpleWebServer
     class SimpleWebServer
@@ -10,5 +11,7 @@ module Rack
         serve(app)
       end
     end
+
+    register :simple_web_server, ::Rack::Handler::SimpleWebServer
   end
 end
